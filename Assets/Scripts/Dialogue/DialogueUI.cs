c6b0bb297f8161c1a5397d7e9a0baf2079b4053c
Yaroslav Oslam <ggStrider1@gmail.com>
2024-11-07 15:47:51 +0200

    Updated dialogue system

--- Assets/Scripts/Dialogue/DialogueUI.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-namespace Dialogue
-{
-    public class DialogueUI : MonoBehaviour
-    {
-    }
-}
--- Assets/Scripts/Player/InputReader.cs
+++ Assets/Scripts/Player/InputReader.cs
@@ -1,19 +1,28 @@
-﻿using Dialogue;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+using Dialogue;
 
 namespace Player
 {
     public class InputReader : MonoBehaviour
     {
         private PlayerMap _playerMap;
+        private DialogueManager _dialogueManager;
 
         public void Initialize(DialogueManager dialogueManager)
         {
             _playerMap = new PlayerMap();
-
-            _playerMap.Main.LMB.started += dialogueManager.NextLine;
+            
+            _dialogueManager = dialogueManager;
+            _playerMap.Main.LMB.started += OnLeftMousePressed;
             
             _playerMap.Enable();
+        }
+
+        private void OnLeftMousePressed(InputAction.CallbackContext context)
+        {
+            _dialogueManager.NextLine();
         }
     }
 }
