f1dd18ebcebb6567c9e4a197f64add37844ec333
Yaroslav Oslam <ggStrider1@gmail.com>
2024-11-24 13:20:56 +0200

    Added ignore layer into param

--- Assets/Scripts/GetObjects/GetObjectByRay.cs
+++ Assets/Scripts/GetObjects/GetObjectByRay.cs
@@ -10,11 +10,12 @@ namespace GetObjects
         /// <param name="position">where ray starts casting</param>
         /// <param name="direction">direction where ray casts</param>
         /// <param name="distance">distance of ray</param>
+        /// <param name="ignoreLayer">layer to not check by ray (skipping obj with this layer)</param>
         /// <returns>GameObject which was in ray or null</returns>
-        public static GameObject Get(Vector3 position, Vector3 direction, float distance)
+        public static GameObject Get(Vector3 position, Vector3 direction, float distance, LayerMask ignoreLayer)
         {
             return Physics.Raycast(position, 
-                direction, out var hitInfo, distance) ? hitInfo.collider.gameObject : null;
+                direction, out var hitInfo, distance, ~ignoreLayer) ? hitInfo.collider.gameObject : null;
         }
     }
 }
