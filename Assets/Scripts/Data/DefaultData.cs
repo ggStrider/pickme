84239e327dac0efbe9fe3e561e67eb175d2fa0c3
Yaroslav Oslam <ggStrider1@gmail.com>
2024-11-24 13:23:34 +0200

    Added component which saves default data (default trigger layer etc)

---
+++ Assets/Scripts/Data/DefaultData.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Data
+{
+    public static class DefaultData
+    {
+        public static readonly LayerMask TriggerLayer = LayerMask.GetMask("Trigger"); 
+    }
+}
