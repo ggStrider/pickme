407890ecbc5289132e59dbfc438a3c1ea9ede295
Yaroslav Oslam <ggStrider1@gmail.com>
2024-11-26 14:28:08 +0200

    Added test component to check press and hold feature

---
+++ Assets/Scripts/Hybrid/PressAndHold/HoldDebug.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Hybrid.PressAndHold;
+
+namespace Hybrid
+{
+    [RequireComponent(typeof(PressAndHoldEvent))]
+    public class HoldDebug : MonoBehaviour, IPressAndHold
+    {
+        private void Start()
+        {
+            GetComponent<PressAndHoldEvent>().SubscribeObserver(this);
+        }
+        
+        public void Completed()
+        {
+            Debug.Log("Completed");
+        }
+    }
+}
